c79e57a649e4aefabde7d75df2c3a2ae9980d593
naylasalma <niaaamela@gmail.com>
2025-05-27 11:56:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈halo,selamat datang di web nayla")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "aplikasi seru"
 )
